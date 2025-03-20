4fe3a48cb96aa9e982422f127eca9239359220d8
Vosik <vosikvos@gmail.com>
2025-03-20 14:33:14 +0200

    e - add gitignore

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Саша Восколович on 20.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
